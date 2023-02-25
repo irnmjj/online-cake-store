8abdbd008489d04e302806d513acbe092407a699
irnmjj <irnmjj@gmail.com>
2023-02-25 11:57:49 +0900

    first commit

---
+++ merry-happy-cake/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Merry Happy Cake</a>
+      </header>
+      <main>cake lists</main>
+    </div>
+  );
+}
+
+export default App;
